5d3ea30c9b590f64698b7d62178ed83a5b81d394
qiaohua <shengyan1985@gmail.com>
2013-08-12 14:14:37 +0800

    U: add treetable

---
+++ treetable/spec.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+  var $ = require("$");
+  var Treetable = require('./src/treetable');
+
+  describe('Treetable', function() {
+    it('should has treetable method', function() {
+      expect($.fn.treetable).to.be.a('function');
+      expect(Treetable.Tree).to.be.a('function');
+    });
+  });
+});
