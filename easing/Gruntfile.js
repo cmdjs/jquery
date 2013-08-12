cd8d14f01428f1d475b8e67fc99ec5ba7cdb7302
qiaohua <shengyan1985@gmail.com>
2013-08-12 16:18:53 +0800

    U: update easing

--- easing/Gruntfile.js
+++ easing/Gruntfile.js
@@ -1,38 +1,30 @@
 module.exports = function(grunt) {
-  var pkg = grunt.file.readJSON('package.json');
+    var pkg = grunt.file.readJSON('package.json');
 
-  grunt.initConfig({
-    pkg: pkg,
+    grunt.initConfig({
+        pkg: pkg,
 
-    download: {
-      options: {
-        dest: 'dist'
-      },
-      src: {
-        options: {
-          header: [
-            'define("<%= pkg.family %>/<%= pkg.name %>/<%= pkg.version %>/easing-debug", ["jquery-debug"], function(require) {',
-            '  var jQuery = require("jquery-debug"); '
-          ].join('\n'),
-          footer: '});'
-        },
-        url: 'https://raw.github.com/danro/jquery-easing/master/jquery.easing.js',
-        name: 'easing-debug.js'
-      },
-      min: {
-        options: {
-          header: [
-            'define("<%= pkg.family %>/<%= pkg.name %>/<%= pkg.version %>/easing", ["jquery"], function(require) {',
-            '  var jQuery = require("jquery"); '
-          ].join('\n'),
-          footer: '});'
-        },
-        url: 'https://raw.github.com/danro/jquery-easing/master/jquery.easing.min.js',
-        name: 'easing.js'
-      }
-    }
-  });
+        download: {
+            options: {
+                dest: 'src'
+            },
+            js: {
+                options: {
+                    transform: function(code) {
+                        return [
+                            "define(function(require, exports, module) {",
+                            "var jQuery = require('$');",
+                            code,
+                            "});"
+                        ].join('\n');
+                    }
+                },
+                url: 'https://raw.github.com/danro/jquery-easing/master/jquery.easing.js',
+                name: 'easing.js'
+            }
+        }
+    });
 
-  grunt.loadTasks('../_tasks/download/tasks');
-  grunt.registerTask('build', ['download']);
+    grunt.loadTasks('../_tasks/download/tasks');
+    grunt.registerTask('default', ['download']);
 };
